78c33317ffb1ac5f2f2f05572a71363b7cde6de2
Alexandre Gaudreau <alexgaudfallout@gmail.com>
2019-11-15 15:34:50 -0500

    Integration du login par sql

--- Superviseur/Superviseur/routes/index.js
+++ Superviseur/Superviseur/routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'malo25030',
+  database : 'PanneauRAM'
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log('Vous êtes connecté à votre BDD');
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('Pages/index', { title: 'Express' });
+});
+
+router.post('/', function(req, res, next){
+  console.log(req.body.username);
+  console.log(req.body.password);
+  var username = req.body.username;
+  var password = req.body.password;
+  var queryString = 'SELECT * FROM Authentification WHERE Username = "'+username+'" AND Password = "'+password+'"';
+  var query = connection.query(queryString, function(err, rows, fields){
+    if(!err){
+      console.log('Ma requête est passe!');
+      res.render('Pages/acceuilAdmin', {})
+    };
+  });
 });
 
 module.exports = router;
