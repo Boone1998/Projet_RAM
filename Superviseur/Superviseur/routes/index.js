ca1404cdad295835f3557cc7599c50c826c6eabc
Alexandre Gaudreau <alexgaudfallout@gmail.com>
2019-11-19 12:05:50 -0500

    Régler bug login

--- Superviseur/Superviseur/routes/index.js
+++ Superviseur/Superviseur/routes/index.js
@@ -23,24 +23,26 @@ router.post('/', function(req, res, next){
   console.log(req.body.password);
   var username = req.body.username;
   var password = req.body.password;
-  var queryString = 'SELECT * FROM Authentification WHERE (Username = "'+username+'") AND (Password = "'+password+'")';
-  var query = connection.query(queryString, function(err, rows, fields){
+  var queryString = 'SELECT * FROM Authentification WHERE (Username = ?) AND (Password = ?)';
+  var query = connection.query(queryString, [username, password], function(err, result){
     if(!err){
       console.log('Ma requête est passe!');
-      console.log(rows);
-      if((username == "admin") && (password == "admin")){
-        res.render('Pages/acceuilAdmin', {username: 'Admin', title: 'Page Admin'});
-      }
-      else if((username == "superviseur") && (password == "superviseur")){
-        res.render('Pages/acceuilSuper', {username: 'Superviseur', title: 'Page Superviseur'});
-      }
-      else if(username == "guest"){
-        res.render('Pages/acceuilGuest', {username: 'Visiteur', title: 'Page Visiteur'});
+      console.log(result);
+      if(result[0]){
+        if((result[0].Username == "admin") && (result[0].Password == "admin")){
+          res.render('Pages/acceuilAdmin', {username: 'Admin', title: 'Page Admin'});
+        }
+        else if((result[0].Username == "operateur") && (result[0].Password == "operateur")){
+          res.render('Pages/acceuilSuper', {username: 'Superviseur', title: 'Page Superviseur'});
+        }
+        else if(result[0].Username == "guest"){
+          res.render('Pages/acceuilGuest', {username: 'Visiteur', title: 'Page Visiteur'});
+        }
       }
       else{
         res.render('Pages/errorLogin');
       }
-    };
+    }
   });
 });
 
