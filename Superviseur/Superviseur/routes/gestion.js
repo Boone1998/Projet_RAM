273b12451410d83d5566a7ec7851a5d0c3b7efa2
Alexandre Gaudreau <alexgaudfallout@gmail.com>
2019-12-06 08:31:42 -0500

    Supprimer, ajouter user

--- Superviseur/Superviseur/routes/gestion.js
+++ Superviseur/Superviseur/routes/gestion.js
@@ -24,4 +24,32 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.post('/', function(req, res) {
+  console.log(req.body.modifUser);
+  if(req.body.modifUser == "Ajout"){
+    var updateSQL = 'INSERT INTO Authentification (Username, Password, NiveauDroit) VALUES ?';
+    var valuesSQL = [[req.body.newUser, req.body.newPass, req.body.Droit]];
+    var query = connection.query(updateSQL,[valuesSQL], function(err, result){
+      if(!err){
+        console.log('1 record inserted');
+      }
+    });
+  }
+  else if(req.body.modifUser == "Delete"){
+    var deleteSQL = 'DELETE FROM Authentification WHERE Username = ?';
+    var deleteValue = req.body.newUser;
+    connection.query(deleteSQL, [deleteValue], function(err, result){
+      if(!err){
+        console.log('1 record deleted');
+      }
+    });
+  }
+  var queryStringDemande = 'SELECT Username, Password, NiveauDroit FROM Authentification';
+  connection.query(queryStringDemande, function(err, result, fields) {
+    if(!err){
+      res.render('Pages/gestion', {user: 'admin', fields: fields, data: result});
+    }
+  });
+});
+
 module.exports = router;
